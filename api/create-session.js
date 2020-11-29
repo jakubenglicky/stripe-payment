ed90593763a462cd15314ff2058b2d84d0d8fb71
jakubenglicky <jakubenglicky@gmail.com>
2020-11-29 10:27:06 +0100

    Create session

---
+++ api/create-session.js
@@ -0,0 +1,18 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
+
+module.exports = (req, res) => {
+
+	const session = stripe.checkout.sessions.create({
+		payment_method_types: ['card'],
+		line_items: [
+			req.body.items,
+		],
+		mode: 'payment',
+		success_url: `${process.env.STRIPE_SUCCESS_URL}`,
+		cancel_url: `${process.env.STRIPE_CANCEL_URL}`,
+	});
+
+	res.json({ id: session.id });
+}
+
+
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET);
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
