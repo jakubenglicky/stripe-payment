10bb92c585c39717cde475c2807838a60e3b23b5
jakubenglicky <jakubenglicky@gmail.com>
2020-11-29 11:13:38 +0100

    Await

--- api/create-session.js
+++ api/create-session.js
@@ -1,7 +1,8 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
-async function createSession(req) {
-	return await stripe.checkout.sessions.create({
+module.exports = async (req, res) => {
+
+	const session = await stripe.checkout.sessions.create({
 		payment_method_types: ['card'],
 		line_items: [
 			req.body.items,
@@ -10,11 +11,7 @@ async function createSession(req) {
 		success_url: `${process.env.STRIPE_SUCCESS_URL}`,
 		cancel_url: `${process.env.STRIPE_CANCEL_URL}`,
 	});
-}
 
-module.exports = (req, res) => {
-
-	const session = createSession(req);
-
+	console.log(session.id);
 	res.json({ id: session.id });
 }
