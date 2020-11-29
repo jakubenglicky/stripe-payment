691bfca1dc0ddf3c4232b644e2b943d1a9342cb0
jakubenglicky <jakubenglicky@gmail.com>
2020-11-29 10:11:38 +0100

    Init commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.static('.'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors())
+
+app.post('/create-session', async (req, res) => {
+
+	const session = await stripe.checkout.sessions.create({
+		payment_method_types: ['card'],
+		line_items: [
+			req.body.items,
+		],
+		mode: 'payment',
+		success_url: `${process.env.STRIPE_SUCCESS_URL}`,
+		cancel_url: `${process.env.STRIPE_CANCEL_URL}`,
+	});
+	res.json({ id: session.id });
+});
+app.listen(4242, () => console.log('Running on port 4242'));
